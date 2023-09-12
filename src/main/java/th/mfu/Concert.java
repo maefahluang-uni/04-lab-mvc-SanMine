49973c12a65d5a15dacce4a65c98224999b8347e
Franco <122962811+SanMine@users.noreply.github.com>
2023-09-12 12:03:33 +0000

    Done

--- src/main/java/th/mfu/Concert.java
+++ src/main/java/th/mfu/Concert.java
@@ -5,80 +5,57 @@ import java.util.Date;
 public class Concert {
 
     //TODO: add attributes
-    private int id; 
-    private String title; 
+    private long id;
+    private String title;
     private String performer;
-    private Date date; 
+    private Date date;
     private String description;
-    
-    public Concert(int id, String title, String performer, Date date, String description) {
-        this.id = id;
-        this.title = title;
-        this.performer = performer;
-        this.date = date;
-        this.description = description;
-    }
-    
-   
-    public int getId() {
-        return id;
-    }
 
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-
-    public String getTitle() {
-        return title;
-    }
-
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-
-    public String getPerformer() {
-        return performer;
-    }
-
-
-    public void setPerformer(String performer) {
-        this.performer = performer;
-    }
-
-
-    public Date getDate() {
-        return date;
-    }
-
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
-
-    public String getDescription() {
-        return description;
-    }
-
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 
 
     public Concert() {
     }
     public Concert(String title, String description) {
         //TODO: set attributes
-        this.title = title;
-        this.description = description;
+        //setTitle(title);
+       // setDescription(description);
+       this.title=title;
+       this.description=description;
     }
  
     //TODO: add getters and setters for all attributes
     // You can use Source action/generate getter setter function
+
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getPerformer() {
+        return performer;
+    }
+    public void setPerformer(String performer) {
+        this.performer = performer;
+    }
+    public Date getDate() {
+        return date;
+    }
+    public void setDate(Date date) {
+        this.date = date;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String dscription) {
+        this.description = dscription;
+    }
 
 }
--- src/main/java/th/mfu/ConcertController.java
+++ src/main/java/th/mfu/ConcertController.java
@@ -7,9 +7,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 
-import javax.xml.crypto.Data;
-
-import org.apache.tomcat.jni.Local;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -23,39 +20,44 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class ConcertController {
     // TODO: create hashmap of concerts for storing data
-        HashMap<Integer ,Concert > db = new HashMap < Integer, Concert >();
-        private int nextId = 1;
+
+     private int nextId=1;
+   private HashMap<Integer,Concert> concerts = new HashMap<Integer,Concert>();
+
     //TODO: add initbinder to convert date
+
     @InitBinder
-    public void initbinder (WebDataBinder binder){
-        SimpleDateFormat dateFormat = new SimpleDateFormat ("yyyy-MM-dd",Locale.US);
-        binder.registerCustomEditor(Data.class, new CustomDateEditor(dateFormat, true));
+    public final void initBinderUsuariosFormValidator(final WebDataBinder binder, final Locale locale) {
+        final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", locale);
+        binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, true));
     }
+
     @GetMapping("/concerts")
     public String listConcerts(Model model) {
         // TODO: add concerts to model
-         model.addAttribute("concerts",db.values());
+        model.addAttribute("concerts", concerts.values());
         // TODO: return a template to list concerts
         return "list-concert";
     }
 
     @GetMapping("/add-concert")
     public String addAConcertForm(Model model) {
+
+      //  Concert addedConcert = new Concert();
         // TODO: pass blank concert to a form
-        Concert con = new Concert();
-        model.addAttribute("concert", con);
+        Concert concert=new Concert();
+        model.addAttribute("concert", concert);
         // TODO: return a template for concert form
         return "add-concert-form";
     }
 
     @PostMapping("/concerts")
     public String saveConcert(@ModelAttribute Concert concert) {
-        concert.setId(nextId);
-        db.put(nextId,concert);
-        nextId++;
-
         // TODO: add concert to list of concerts
+        concert.setId(nextId);
+        concerts.put(nextId,concert);
         // TODO: increment nextId
+        nextId++;
         // TODO: redirect to list concerts
         return "redirect:/concerts";
     }
@@ -63,7 +65,7 @@ public class ConcertController {
     @GetMapping("/delete-concert/{id}")
     public String deleteConcert(@PathVariable int id) {
         // TODO: remove concert from list of concerts
-        db.remove(id);
+        concerts.remove(id);
         // TODO: redirect to list concerts
         return "redirect:/concerts";
     }
@@ -72,7 +74,8 @@ public class ConcertController {
     @GetMapping("/delete-concert")
     public String removeAllConcerts() {
         //TODO: clear all employees and reset id
-        db.clear();
+        concerts.clear();
+        nextId=1;
         // TODO: redirect to list concerts
         return "redirect:/concerts";
     }
